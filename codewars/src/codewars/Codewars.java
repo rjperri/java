50a6012b7b397cf56d9d04dd7ea040d611e160e3
Bob Perri <rjperri@gmail.com>
2016-03-07 14:43:06 -0500

    Solution for windInfo codewar

---
+++ codewars/src/codewars/Codewars.java
@@ -0,0 +1,16 @@
+package codewars;
+
+/**
+ *
+ * @author rjperri
+ */
+public class Codewars {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ codewars/src/codewars/WindInfo.java
@@ -0,0 +1,35 @@
+package codewars;
+import java.lang.Math.*;
+
+/**
+ *
+ * @author rjperri
+ */
+public class WindInfo {
+    
+    public static String message(String rwy, int windDirection, int windSpeed) {
+        int headingOffset = 360 - extractHeading(rwy);
+        double headWind =  getWindHeadWind(windDirection, windSpeed, headingOffset);
+        double crosswind = getWindCrossWind(windDirection, windSpeed, headingOffset);
+        
+        String hwDirection = (headWind >= 0) ? "Headwind" : "Tailwind"; 
+        String cwDirectin = (crosswind >= 0) ? "right" : "left"; 
+        
+        return hwDirection + " " + (int) Math.abs(headWind)  + " knots. Crosswind " + (int) Math.abs(crosswind) + " knots from your " + cwDirectin + "."; 
+    }
+    
+    protected static int extractHeading(String heading) {
+        String intValue = heading.replaceAll("[^0-9]", "");
+        return Integer.parseInt(intValue) * 10;
+    }
+    
+    protected static double getWindHeadWind(int windDirection, int windSpeed, int headingOffset) {
+        return Math.round(Math.cos(Math.toRadians(windDirection + headingOffset)) * windSpeed);
+    }
+
+    protected static double getWindCrossWind(int windDirection, int windSpeed, int headingOffset) {
+        return Math.round(Math.sin(Math.toRadians(windDirection + headingOffset)) * windSpeed);
+    }
+    
+  
+}
